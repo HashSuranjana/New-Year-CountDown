1fc9d5edf5cf9cd9440012d8930eeb5fbfe168b0
HashSuranjana <hasithasuranjana12@gmail.com>
2024-04-19 20:37:16 +0530

    Updated files

--- CountDown.js
+++ CountDown.js
@@ -28,7 +28,6 @@ let countdown = () => {
 
     day = date.getDate() ; 
 
-    console.log(day);
     month = date.getMonth() + 1 ; // getMonth gives 0 to 11 every month. so +1 gives real numbering of the month
 
     document.getElementById('seconds-r').innerHTML = 60 - date.getSeconds() ;   //gives remaining seconds
@@ -37,34 +36,80 @@ let countdown = () => {
 
     document.getElementById('hours-r').innerHTML = 24 - date.getHours() + " : " ; //gives remaining hours
 
-    console.log(month);
-
-    switch(month){
+    switch(8){
 
         case 1 :
+
             document.getElementById('days-r').innerHTML = days - day + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/January.jpg')" ;
+            break;
+
         case 2 :
+
             document.getElementById('days-r').innerHTML = days - (day + 31) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/February.jpg')" ;
+            break;
+
         case 3 :
+
             document.getElementById('days-r').innerHTML = days - ( daysfeb + day + 31) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/march.jpg')" ;
+            break;
+
         case 4 :
+
             document.getElementById('days-r').innerHTML = days - (day + daysfeb + 62) + " : "  ;
+            document.querySelector('body').style.backgroundImage = "url('Images/april.jpg')" ;
+            break;
+
         case 5 :
+
             document.getElementById('days-r').innerHTML = days - (day + daysfeb + 92) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/may.jpg')" ;
+            break;
+
         case 6 :
+
             document.getElementById('days-r').innerHTML = days - (day + daysfeb + 123) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/april.jpg')" ;
+            break;
+
         case 7 :
+
             document.getElementById('days-r').innerHTML = days - (day + daysfeb + 153) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/july.jpg')" ;
+            break;
+
         case 8 :
+
             document.getElementById('days-r').innerHTML = days - (day + daysfeb + 184) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/august.jpg')" ;
+            break;
+
         case 9 :
+
             document.getElementById('days-r').innerHTML = days - (day + daysfeb + 215) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/april.jpg')" ;
+            break;
+
         case 10 :
+
             document.getElementById('days-r').innerHTML = days - (day + daysfeb + 245) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/april.jpg')" ;
+            break;
+
         case 11 :
+
             document.getElementById('days-r').innerHTML = days - (day + daysfeb + 276) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/april.jpg')" ;
+            break;
+
         case 12 :
+
             document.getElementById('days-r').innerHTML = days - (day + daysfeb + 306) + " : " ;
+            document.querySelector('body').style.backgroundImage = "url('Images/april.jpg')" ;
+            break;
+
 
     }
 
