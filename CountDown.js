488ab6e64321b4e247f6ee1f206e05421dafacec
HashSuranjana <hasithasuranjana12@gmail.com>
2024-04-19 20:21:03 +0530

    updated files

--- CountDown.js
+++ CountDown.js
@@ -4,22 +4,22 @@ let current_Year = get_year.getFullYear() + 1; //initialize the upcoming year
 
 document.getElementById('note').innerHTML = "New Year " + current_Year + " !" ;
 
-let day , month , Days , daysfeb; // initialize the variables
+let day , month , days , daysfeb; // initialize the variables
 
 //function to countdown the time
 
 let countdown = () => {
 
-    //check whether current year is a normal year or 
-    if(current_Year % 4 == 0) {
+    //check whether current year is a normal year or leap year 
+    if((current_Year -1) % 4 == 0) {
         
-        Days = 366 ;
+        days = 366 ;
         daysfeb = 29 ;
     }
 
     else {
 
-        Days = 365 ;
+        days = 365 ;
         daysfeb = 28 ;
     }
 
@@ -28,6 +28,7 @@ let countdown = () => {
 
     day = date.getDate() ; 
 
+    console.log(day);
     month = date.getMonth() + 1 ; // getMonth gives 0 to 11 every month. so +1 gives real numbering of the month
 
     document.getElementById('seconds-r').innerHTML = 60 - date.getSeconds() ;   //gives remaining seconds
@@ -36,10 +37,34 @@ let countdown = () => {
 
     document.getElementById('hours-r').innerHTML = 24 - date.getHours() + " : " ; //gives remaining hours
 
+    console.log(month);
+
     switch(month){
 
         case 1 :
-            document.getElementById('days-r').innerHTML = 
+            document.getElementById('days-r').innerHTML = days - day + " : " ;
+        case 2 :
+            document.getElementById('days-r').innerHTML = days - (day + 31) + " : " ;
+        case 3 :
+            document.getElementById('days-r').innerHTML = days - ( daysfeb + day + 31) + " : " ;
+        case 4 :
+            document.getElementById('days-r').innerHTML = days - (day + daysfeb + 62) + " : "  ;
+        case 5 :
+            document.getElementById('days-r').innerHTML = days - (day + daysfeb + 92) + " : " ;
+        case 6 :
+            document.getElementById('days-r').innerHTML = days - (day + daysfeb + 123) + " : " ;
+        case 7 :
+            document.getElementById('days-r').innerHTML = days - (day + daysfeb + 153) + " : " ;
+        case 8 :
+            document.getElementById('days-r').innerHTML = days - (day + daysfeb + 184) + " : " ;
+        case 9 :
+            document.getElementById('days-r').innerHTML = days - (day + daysfeb + 215) + " : " ;
+        case 10 :
+            document.getElementById('days-r').innerHTML = days - (day + daysfeb + 245) + " : " ;
+        case 11 :
+            document.getElementById('days-r').innerHTML = days - (day + daysfeb + 276) + " : " ;
+        case 12 :
+            document.getElementById('days-r').innerHTML = days - (day + daysfeb + 306) + " : " ;
 
     }
 
