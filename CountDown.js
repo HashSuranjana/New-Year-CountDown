3437da59a01edfd47d2cc829066084f2d2cee71a
HashSuranjana <hasithasuranjana12@gmail.com>
2024-04-19 19:23:56 +0530

    updated files

---
+++ CountDown.js
@@ -0,0 +1,7 @@
+let date = new Date() ;
+
+let current_Year = date.getFullYear() + 1; //initialize the upcoming year
+
+document.getElementById('note').innerHTML = "New Year " + current_Year ;
+
+let day , hours, miutes, seconds ; // initialize the variables
